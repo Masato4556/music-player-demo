c7acfe92f68c87754a5920cd47e2542a56c7bd8e
Otsuka Masato <ootsukamase@gmail.com>
2025-03-24 01:51:15 +0900

    音声プレーヤーの進行状況を表示するためのスライダーコンポーネントを追加し、音声の再生位置をシークできる機能を実装

--- src/renderer/src/components/AudioPlayer/AudioContext.tsx
+++ src/renderer/src/components/AudioPlayer/AudioContext.tsx
@@ -1,32 +1,60 @@
-import React, { createContext, useContext, useRef } from 'react'
+import React, { createContext, useContext, useRef, useState, useEffect, useCallback } from 'react'
 
 type AudioContextType = {
   setAudioSrc: (src: string) => void
   play: () => void
   pause: () => void
   stop: () => void
+  currentTime: number
+  duration: number
+  seek: (time: number) => void
 }
 
 const AudioContext = createContext<AudioContextType | undefined>(undefined)
 
 export const AudioProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const audioRef = useRef<HTMLAudioElement>(new Audio())
-  const setAudioSrc = (src: string) => {
-    audioRef.current.src = src
-  }
+  const [currentTime, setCurrentTime] = useState(0)
+  const [duration, setDuration] = useState(0)
+
+  const setAudioSrc = useCallback(
+    (src: string) => {
+      audioRef.current.src = src
+      audioRef.current.onloadedmetadata = () => {
+        setDuration(audioRef.current.duration)
+      }
+    },
+    [setDuration]
+  )
+
   const play = () => {
     audioRef.current.play()
   }
+
   const pause = () => {
     audioRef.current.pause()
   }
+
   const stop = () => {
     audioRef.current.pause()
     audioRef.current.currentTime = 0
+    setCurrentTime(0)
   }
 
+  const seek = (time: number) => {
+    audioRef.current.currentTime = time
+    setCurrentTime(time)
+  }
+
+  useEffect(() => {
+    const updateTime = async () => {
+      setCurrentTime(audioRef.current.currentTime)
+    }
+    audioRef.current.addEventListener('timeupdate', updateTime)
+  }, [])
+
   return (
-    <AudioContext.Provider value={{ setAudioSrc, play, pause, stop }}>
+    <AudioContext.Provider value={{ setAudioSrc, play, pause, stop, currentTime, duration, seek }}>
       {children}
     </AudioContext.Provider>
   )
@@ -48,4 +76,9 @@ export const useAudioControls = (): Pick<AudioContextType, 'play' | 'pause' | 's
 export const useSetAudio = (): Pick<AudioContextType, 'setAudioSrc'> => {
   const { setAudioSrc } = useAudio()
   return { setAudioSrc }
+}
+
+export const useAudioProgress = (): Pick<AudioContextType, 'currentTime' | 'duration' | 'seek'> => {
+  const { currentTime, duration, seek } = useAudio()
+  return { currentTime, duration, seek }
 }
--- src/renderer/src/components/AudioPlayer/ProgressBar.tsx
+++ src/renderer/src/components/AudioPlayer/ProgressBar.tsx
@@ -1,5 +1,33 @@
-import { Progress } from '../ui/progress'
+import { cn } from '@renderer/lib/utils'
+import { Slider } from '../ui/slider'
+import { useAudioProgress } from './AudioContext'
 
 export const ProgressBar = (): JSX.Element => {
-  return <Progress value={0} className="w-[60%]" />
+  const { currentTime, duration, seek } = useAudioProgress()
+
+  const handleSeek = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('event.target.value', event.target.value)
+    const newTime = (Number(event.target.value) / 100) * duration
+    seek(newTime)
+  }
+  const onClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const slider = event.currentTarget.getBoundingClientRect()
+    const clickPosition = event.clientX - slider.left
+    const newValue = (clickPosition / slider.width) * 100
+    const newTime = (newValue / 100) * duration
+    seek(newTime)
+  }
+
+  return (
+    <div className="w-[60%]">
+      <Slider
+        value={[(currentTime / duration) * 100 || 0]}
+        min={0}
+        max={100}
+        onChange={handleSeek}
+        onClick={onClick}
+        className={cn('w-full')}
+      />
+    </div>
+  )
 }
