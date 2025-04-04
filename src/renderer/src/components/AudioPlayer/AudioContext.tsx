304335706a3232e1ab5eb82352fb34b5c995cd1e
Otsuka Masato <ootsukamase@gmail.com>
2025-04-04 17:57:08 +0900

    オーディオ操作周りのロジックを分離

--- src/renderer/src/components/AudioPlayer/AudioContext.tsx
+++ src/renderer/src/components/AudioPlayer/AudioContext.tsx
@@ -1,5 +1,6 @@
 import { Seconds } from '@renderer/components/AudioPlayer/Seconds'
-import React, { createContext, useContext, useRef, useState, useCallback } from 'react'
+import React, { createContext, useContext } from 'react'
+import { useAudio } from './useAudio'
 
 type AudioContextType = {
   setAudio: (src: string) => void
@@ -15,62 +16,7 @@ type AudioContextType = {
 const AudioContext = createContext<AudioContextType | undefined>(undefined)
 
 export const AudioProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const audioRef = useRef<HTMLAudioElement>(new Audio())
-  const [currentTime, setCurrentTime] = useState(new Seconds(0))
-  const [duration, setDuration] = useState(new Seconds(0))
-
-  const setAudio = useCallback(
-    (filePath: string) => {
-      audioRef.current.pause()
-      audioRef.current = new Audio(filePath)
-      audioRef.current.onloadedmetadata = () => {
-        setDuration(new Seconds(audioRef.current.duration))
-      }
-      audioRef.current.onerror = (error) => {
-        console.error('Audio load error:', error)
-      }
-      const updateTime = async () => {
-        setCurrentTime(new Seconds(audioRef.current.currentTime))
-      }
-      audioRef.current.addEventListener('timeupdate', updateTime)
-      audioRef.current.load() // ロードを強制的に開始
-    },
-    [setDuration]
-  )
-
-  const openAudio = useCallback(async () => {
-    // TODO: blobURLに変換せずに直接srcに設定できないか検討する
-    const filePath = await window.api.openFile()
-    console.log(filePath)
-    if (filePath === null) return
-
-    const audioData = await window.api.readFile(filePath)
-    if (audioData === null) return
-
-    const blob = new Blob([audioData], { type: 'audio/mpeg' })
-    const url = URL.createObjectURL(blob)
-
-    setAudio(url)
-  }, [setAudio])
-
-  const play = () => {
-    audioRef.current.play()
-  }
-
-  const pause = () => {
-    audioRef.current.pause()
-  }
-
-  const stop = () => {
-    audioRef.current.pause()
-    audioRef.current.currentTime = 0
-    setCurrentTime(new Seconds(0))
-  }
-
-  const seek = (time: number) => {
-    audioRef.current.currentTime = time
-    setCurrentTime(new Seconds(time))
-  }
+  const { setAudio, openAudio, play, pause, stop, currentTime, duration, seek } = useAudio()
 
   return (
     <AudioContext.Provider
@@ -81,25 +27,25 @@ export const AudioProvider: React.FC<{ children: React.ReactNode }> = ({ childre
   )
 }
 
-export const useAudio = (): AudioContextType => {
+export const useAudioContext = (): AudioContextType => {
   const context = useContext(AudioContext)
   if (!context) {
-    throw new Error('useAudio must be used within an AudioProvider')
+    throw new Error('useAudioContext must be used within an AudioProvider')
   }
   return context
 }
 
 export const useAudioControls = (): Pick<AudioContextType, 'play' | 'pause' | 'stop'> => {
-  const { play, pause, stop } = useAudio()
+  const { play, pause, stop } = useAudioContext()
   return { play, pause, stop }
 }
 
 export const useSetAudio = (): Pick<AudioContextType, 'setAudio' | 'openAudio'> => {
-  const { setAudio, openAudio } = useAudio()
+  const { setAudio, openAudio } = useAudioContext()
   return { setAudio, openAudio }
 }
 
 export const useAudioProgress = (): Pick<AudioContextType, 'currentTime' | 'duration' | 'seek'> => {
-  const { currentTime, duration, seek } = useAudio()
+  const { currentTime, duration, seek } = useAudioContext()
   return { currentTime, duration, seek }
 }
---
+++ src/renderer/src/components/AudioPlayer/useAudio.ts
@@ -0,0 +1,72 @@
+import { useRef, useState, useCallback } from 'react'
+import { Seconds } from './Seconds'
+
+export const useAudio = () => {
+  const audioRef = useRef<HTMLAudioElement>(new Audio())
+  const [currentTime, setCurrentTime] = useState(new Seconds(0))
+  const [duration, setDuration] = useState(new Seconds(0))
+
+  const setAudio = useCallback(
+    (filePath: string) => {
+      audioRef.current.pause()
+      audioRef.current = new Audio(filePath)
+      audioRef.current.onloadedmetadata = () => {
+        setDuration(new Seconds(audioRef.current.duration))
+      }
+      audioRef.current.onerror = (error) => {
+        console.error('Audio load error:', error)
+      }
+      const updateTime = async () => {
+        setCurrentTime(new Seconds(audioRef.current.currentTime))
+      }
+      audioRef.current.addEventListener('timeupdate', updateTime)
+      audioRef.current.load() // ロードを強制的に開始
+    },
+    [setDuration]
+  )
+
+  const openAudio = useCallback(async () => {
+    // TODO: blobURLに変換せずに直接srcに設定できないか検討する
+    const filePath = await window.api.openFile()
+    console.log(filePath)
+    if (filePath === null) return
+
+    const audioData = await window.api.readFile(filePath)
+    if (audioData === null) return
+
+    const blob = new Blob([audioData], { type: 'audio/mpeg' })
+    const url = URL.createObjectURL(blob)
+
+    setAudio(url)
+  }, [setAudio])
+
+  const play = () => {
+    audioRef.current.play()
+  }
+
+  const pause = () => {
+    audioRef.current.pause()
+  }
+
+  const stop = () => {
+    audioRef.current.pause()
+    audioRef.current.currentTime = 0
+    setCurrentTime(new Seconds(0))
+  }
+
+  const seek = (time: number) => {
+    audioRef.current.currentTime = time
+    setCurrentTime(new Seconds(time))
+  }
+
+  return {
+    setAudio,
+    openAudio,
+    play,
+    pause,
+    stop,
+    currentTime,
+    duration,
+    seek
+  }
+}
